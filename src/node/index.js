9f1a6799c83366da16edf36028f97dbd7502fb1e
us999lilili <iam.ueriiiii23@gmail.com>
2025-08-25 10:31:26 +0900

    .github/workflows/develop.ymlと src/node/index.js微修正

--- src/node/index.js
+++ src/node/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 const { Pool } = require("pg");
 const pool = new Pool({
   user: "user_4977",
-  host: "db",
+  host: "postgres",
   database: "crm_4977",
   password: "pass_4977",
   port: 5432,
